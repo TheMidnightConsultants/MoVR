a2252953fc23c5db83b9d1e02960e0e8989af649
Kienan Knight-Boehm <kienan@kienankb.com>
2016-11-03 01:03:11 -0700

    Login now carries over to app page (in title at least); logout button now works

--- editor/urls.py
+++ editor/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 app_name = 'editor'
 urlpatterns = [
-	url(r'^login/', views.login, name='login'),
+	url(r'^login/', views.loginPage, name='loginPage'),
 	url(r'^register/', views.register, name='register'),
 	url(r'^app/', views.app, name='app'),
 	url(r'^mobile/', views.mobile, name='mobile'),
 	url(r'^createUser/', views.createUser, name='createUser'),
+	url(r'^loginAttempt/', views.loginAttempt, name='loginAttempt'),
+	url(r'^logoutAttempt', views.logoutAttempt, name='logoutAttempt'),
 ]
--- editor/views.py
+++ editor/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 # Create your views here.
 
-def login(request):
+def loginPage(request):
 	return render(request, 'editor/login.html')
 
 def register(request):
 	return render(request, 'editor/register.html')
 
 def app(request):
-	return render(request, 'editor/app.html')
+	print "[DEBUG] User from request:", request.user, request.user.username
+	return render(request, 'editor/app.html', {'user': request.user})
 	
 def mobile(request):
 	return render(request, 'editor/mobile.html')
@@ -19,4 +21,18 @@ def mobile(request):
 def createUser(request):
 	user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
 	user.save()
-	return HttpResponseRedirect('/login')
+	return HttpResponseRedirect('/login/')
+
+def loginAttempt(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		login(request, user)
+		return HttpResponseRedirect('/app/')
+	else:
+		return HttpResponseRedirect('/login/')
+
+def logoutAttempt(request):
+	logout(request)
+	return HttpResponseRedirect('/login/')
