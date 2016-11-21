e363701fd6f888f83ff70e69faf01b870419a9bf
Alan <mayhea@Tannen.fifteenth>
2016-11-21 02:26:52 -0500

    Cleaned up some stuff in Scene.js

--- editor/static/editor/js/Scene.js
+++ editor/static/editor/js/Scene.js
@@ -16,64 +16,11 @@ function Scene(){
 
 	this.raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
 
-	/*
-	// floor
-
-	geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
-	geometry.rotateX( - Math.PI / 2 );
-
-	for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
-
-		var vertex = geometry.vertices[ i ];
-		vertex.x += Math.random() * 20 - 10;
-		// vertex.y += Math.random() * 2;
-		vertex.z += Math.random() * 20 - 10;
-
-	}
-
-	for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
-
-		var face = geometry.faces[ i ];
-		face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-		face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-		face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-
-	}
-
-	material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-
-	mesh = new THREE.Mesh( geometry, material );
-	mesh.name = "floorplane";
-	this.scene.add( mesh );
-	*/
-
-	
-//	this.room = new Room(120, 120, 50, 0xAA2288);
-//	this.scene.add(this.room.mesh);
-
-	// object loader
-	
-	var onProgress = function ( xhr ) {
-		if ( xhr.lengthComputable ) {
-			var percentComplete = xhr.loaded / xhr.total * 100;
-			console.log( Math.round(percentComplete, 2) + '% downloaded' );
-		}
-	};
-
-	var onError = function ( xhr ) {
-	};
-	
-
-
-	//
-
 	this.renderer = new THREE.WebGLRenderer();
 	this.renderer.setClearColor( 0xffffff );
 	this.renderer.setPixelRatio( window.devicePixelRatio );
 	this.renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( this.renderer.domElement );
-
-	//
 
 	window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
 };
